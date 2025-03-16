26b1594099a7f854c0390a0cc3c6e57b6f95c032
Yashwant937363 <yashwantpoyrekar@gmail.com>
2025-03-16 22:35:45 +0530

    Completed

--- app/src/main/java/com/example/android_programming_tybscit/MainActivity.kt
+++ app/src/main/java/com/example/android_programming_tybscit/MainActivity.kt
@@ -1,10 +1,15 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.android_programming_tybscit
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val selectItem = BottomNavigationView.OnNavigationItemSelectedListener {
+            item ->
+                if(item.itemId == R.id.videos){
+                    openFragment(VideoFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                else if(item.itemId == R.id.quiz) {
+                    openFragment(QuizFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                else if(item.itemId == R.id.notes) {
+                    openFragment(NotesFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                return@OnNavigationItemSelectedListener false
+        }
+        findViewById<BottomNavigationView>(R.id.navigation).setOnNavigationItemSelectedListener(selectItem)
+    }
+    private fun openFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
     }
 }
